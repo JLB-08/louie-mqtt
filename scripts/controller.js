6e7d9ab0586ebca5fdc5e2e125ffd13c22eec7d9
unknown <2ndyrGroupA@PN26.pn.local>
2019-09-18 08:44:54 +0800

    Initial commit

--- scripts/controller.js
+++ scripts/controller.js
@@ -1,30 +1,23 @@
-// basic functionalities
+var topic = document.getElementById('pub');
+var publish = document.getElementById('pubP');
+var d = new Date();
+
+
 function connectFunc(){
   console.log("Connecting..");
-  var para = document.createElement("p");
-  var node = document.createTextNode("connecting...");
-  para.appendChild(node);
-  var element = document.getElementById("div1");
-  element.appendChild(para);
-
 	// client = mqtt.connect("ws://broker.hivemq.com:8000/mqtt")
 	client = mqtt.connect(document.getElementById('broker').value)
   console.log(document.getElementById('broker').value);
   
 	client.on("connect", function(){
     console.log("Successfully connected");
-    
-  var para = document.createElement("p");
-  var node = document.createTextNode("Successfully connected");
-  para.appendChild(node);
-  var element = document.getElementById("div1");
-  element.appendChild(para);
+    document.getElementById("p").innerHTML = "Connected";
 	})
   
 	client.on("message", function (topic, payload) {
 	  // console.log([topic, payload].join(": "));
 	  console.log("Received { topic: " + topic + "; payload: " + payload + " }");
-	  // client.end();
+    // client.end();
   })
 
   }
@@ -34,8 +27,16 @@ function connectFunc(){
 	client.publish(document.getElementById('pub').value, document.getElementById('pubP').value)
 	console.log("Published { topic: " + document.getElementById('pub').value
 	+ "; payload: " + document.getElementById('pubP').value + " }");
-	// console.log(document.getElementById('pub-topic').value);
-	// console.log(document.getElementById('pub-payload').value);
+
+   var table = document.getElementById("table");
+   var row = table.insertRow(-1);
+   var cell1 = row.insertCell(0);
+   var cell2 = row.insertCell(1);
+   var cell3 = row.insertCell(2);
+   cell1.innerHTML = topic.value;
+   cell2.innerHTML = publish.value;
+   cell3.innerHTML = d;
+
   }
   
   function subscribeFunc(){
@@ -47,30 +48,17 @@ function connectFunc(){
 
   function disconnect(){
   client.end();
-  var para = document.createElement("p");
-  var node = document.createTextNode("");
-  para.appendChild(node);
-  var element = document.getElementById("div1");
-  element.appendChild(para);
+  console.log("Disconnected");
+  document.getElementById("p").innerHTML = "Status";
   }
 
-  client.on("message", function (topic, payload) {
-    let getTopic = topic.toString().slice(5);
-    var stamp = new Date($.now());
-    let parent = $("#tbodyContainer");
-    let row = $("<tr></tr>");
-    let top = $("<th></th>").text(getTopic);
-    let payld = $("<td></td>").text(payload);
-    let time = $("<td></td>").text(stamp.toString().slice(0, 24));
 
-    top.attr("scope", "row");
-    row.attr("id", rownum);
 
-    parent.append(row);
-    $("#" + rownum).append(top, payld, time);
-    rownum += 1;
-  });
 
+
+
+
+ 
 
 
   
